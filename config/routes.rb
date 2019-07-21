c274c3d44c132ff9582c9b276d6512f8b4cb58f1
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-07-21 11:24:36 +0100

    adds account activation

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
+  # get '/email_activation/:id', to: 'account_activation#edit'
 end
